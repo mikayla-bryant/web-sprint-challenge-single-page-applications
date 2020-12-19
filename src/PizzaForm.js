4b079497c15207ba5329b87a52451670f6222a25
mikayla-bryant <mikaylabryant25@gmail.com>
2020-12-18 19:56:34 -0500

    Added all form inputs

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -14,6 +14,31 @@ const PizzaForm = () => {
           <option value='4'>Extra-Large (16 in)</option>
         </select>
       </label>
+
+      <section>
+        <p>Select toppings:</p>
+        <div>
+          <input type='checkbox' id='pepperoni' name='pepperoni' />
+          <label for='pepperoni'>Pepperoni</label>
+        </div>
+        <div>
+          <input type='checkbox' id='onions' name='onions' />
+          <label for='onions'>Onions</label>
+        </div>
+        <div>
+          <input type='checkbox' id='mushrooms' name='mushrooms' />
+          <label for='mushrooms'>Mushrooms</label>
+        </div>
+        <div>
+          <input type='checkbox' id='bell-peppers' name='bell-peppers' />
+          <label for='bell-peppers'>Bell Peppers</label>
+        </div>
+      </section>
+      <label>
+        Special instructions
+        <textarea placeholder='Type special instructions here' />
+      </label>
+      <button>Add to Cart</button>
     </div>
   );
 };
